62e952309cd5fc6d5753d68f3d1212ff8164d364
eralparda <eralparda77@gmail.com>
2026-06-22 15:27:39 +0300

    perfume request added

---
+++ src/main/java/com/eralparda/PerfumeWeb/DTO/PerfumeRequest.java
@@ -0,0 +1,10 @@
+package com.eralparda.PerfumeWeb.DTO;
+
+import lombok.Data;
+
+@Data
+public class PerfumeRequest {
+    private String name;
+    private String brand;
+    private String description;
+}
