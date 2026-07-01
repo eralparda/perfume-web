13fb26b4f59e7addb431586e7ec0a3af9713c03f
eralparda <eralparda77@gmail.com>
2026-07-01 15:22:21 +0300

    cart controller fix

--- src/main/java/com/eralparda/PerfumeWeb/Controller/CartController.java
+++ src/main/java/com/eralparda/PerfumeWeb/Controller/CartController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/cart")
+@RequestMapping("/carts")
 @RequiredArgsConstructor
 public class CartController {
 
